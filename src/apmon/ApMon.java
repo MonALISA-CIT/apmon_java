bea2c3b7783e35bfad38a0d71bbafe46f062a080
Costin Grigoras <costin.grigoras@cern.ch>
2017-03-21 11:13:09 +0100

    Fix splitting large sets of parameters in multiple datagrams

--- src/apmon/ApMon.java
+++ src/apmon/ApMon.java
@@ -1280,7 +1280,7 @@ public class ApMon {
             xdrOS.pad();
             xdrOS.writeString(nodeName);
             xdrOS.pad();
-            xdrOS.writeInt(nParams);
+            xdrOS.writeInt(nParams-start);
             xdrOS.pad();
 
             Object oValue;
